abed8a25002592ec2980b2e6960d0e00757137fb
moaz-elesawey <mohammedmiaz3141@gmail.com>
2022-09-29 09:43:12 +0200

    added test cases for linear methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using MLOptm
 using Test
 
 @testset "MLOptm.jl" begin
-    # Write your tests here.
+
+    f   = (x) -> x^2;
+    ∇f  = (x) -> 2x;
+    ∇²f = (x) -> 2;
+
+    @test isapprox(Minimize!(Golden(f, -10, 10))    , 0.0; atol=1e-4, rtol=0);
+    @test isapprox(Minimize!(BiSection(∇f, -10, 10)), 0.0; atol=1e-4, rtol=0);
+    @test isapprox(Minimize!(Newton(∇f, ∇²f, -0.1)) , 0.0; atol=1e-4, rtol=0);
+    @test isapprox(Minimize!(Secant(∇f, -1.0, -0.9)), 0.0; atol=1e-4, rtol=0);
+
 end
+
